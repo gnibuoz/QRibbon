275e0369d31d8535331b34c28cd9a559912af238
gnibuoz <904738226@qq.com>
2020-06-26 23:16:15 +0800

    上传示例代码

---
+++ Demo/main.cpp
@@ -0,0 +1,12 @@
+﻿#pragma execution_character_set("utf-8")
+
+#include "QtWidgets/qapplication.h"
+#include "vgribbonwindow.h"
+
+int main(int argc, char** argv)
+{
+	QApplication a(argc, argv);
+	VGRibbonWindow w;
+	w.show();
+	return a.exec();
+}
---
+++ Demo/vgribbonwindow.cpp
@@ -0,0 +1,54 @@
+﻿#include "vgribbonwindow.h"
+#include "ui_vgribbonwindow.h"
+
+#include <QShortcut>
+
+#include "../QRibbon/QRibbon.h"
+
+VGRibbonWindow::VGRibbonWindow(QWidget *parent)
+	: QMainWindow(parent)
+{
+	ui = new Ui::VGRibbonWindow();
+	ui->setupUi(this);
+
+	setCorner(Qt::BottomLeftCorner, Qt::LeftDockWidgetArea);
+	setCorner(Qt::BottomRightCorner, Qt::RightDockWidgetArea);
+
+	this->addAction(ui->actionFullScreen);
+	connect(ui->actionFullScreen, &QAction::triggered, [&]() {
+
+		auto full = ui->actionFullScreen->isChecked();
+		menuBar()->setVisible(!full);
+		ui->actionFullScreen->setShortcut(  full ? QKeySequence("Esc") : QKeySequence("Ctrl+F"));
+		
+		static bool maximized = false;// 记录当前状态
+		if ( full )
+		{
+			maximized = isMaximized();
+		}
+		else if ( maximized && isMaximized() )
+		{
+			return;
+		}
+
+		if ( full && !isMaximized() || !full && isMaximized() )
+		{
+			if (isMaximized())
+			{
+				showNormal();
+			}
+			else
+				showMaximized();
+		}
+	});
+
+	connect(ui->pushButton, &QPushButton::clicked, [this]() {
+		QRibbon::install(this);
+		this->show();
+	});
+}
+
+VGRibbonWindow::~VGRibbonWindow()
+{
+	delete ui;
+}
